c7516acbd6d713101d20fbebbc64367897a7c279
Josh Guyette <joshguyettejobs@live.com>
2018-03-13 05:30:02 -0500

    update

--- js/site.js/location.js
+++ js/site.js/location.js
@@ -5,50 +5,45 @@ function getLocationInformation(apiKey, callback) {
     $.getJSON('//api.ipify.org?format=jsonp&callback=?', data => {
         // Get the client's IP based location data
         $.getJSON('//geoip.nekudo.com/api/' + data.ip, data => {
-            // Find the client's local IP address
-            var findIP = new Promise(r => { var w = window, a = new (w.RTCPeerConnection || w.mozRTCPeerConnection || w.webkitRTCPeerConnection)({ iceServers: [] }), b = () => { }; a.createDataChannel(""); a.createOffer(c => a.setLocalDescription(c, b, b), b); a.onicecandidate = c => { try { c.candidate.candidate.match(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g).forEach(r) } catch (e) { } } })
-            findIP.then(ip => {
-                data.local_ip = ip;
-                // Find location details from Google Geocode
-                $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + data.location.latitude + ',' + data.location.longitude + '&key=' + apiKey, geoData => {
+            // Find location details from Google Geocode
+            $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + data.location.latitude + ',' + data.location.longitude + '&key=' + apiKey, geoData => {
                 //console.log(JSON.stringify(geoData, null, 2));    
                 var address_components = geoData.results[0]["address_components"];
-                    $.each(address_components, field => {
-                        //console.log(JSON.stringify(address_components[field], null, 2));
-                        var isPostalCode = (address_components[field].types.indexOf("postal_code") > -1);
-                        var isRegion = (address_components[field].types.indexOf("administrative_area_level_1") > -1);
-                        if (isPostalCode) {
-                            data.postal_code = address_components[field].long_name;
-                        }
-                        if (isRegion) {
-                            data.region = { };
-                            data.region.name = address_components[field].long_name;
-                            data.region.code = address_components[field].short_name;
-                        }
-                    });
-                    // The formatted address
-                    data.formatted_address = geoData.results[0]["formatted_address"];
-                    // Get the location's weather conditions
-                    $.getJSON('https://fcc-weather-api.glitch.me/api/current?lon=' + data.location.longitude + '&lat=' + data.location.latitude, weatherData => {
-                        console.log(JSON.stringify(weatherData, null, 2));  
-                        data.weather = weatherData.main;
-                        data.weather.temp = {
-                            celsius: weatherData.main.temp,
-                            kelvin: (273 + weatherData.main.temp),
-                            fahrenheit: (weatherData.main.temp * 9 / 5 + 32)
-                        };
-                        data.weather.visibility = weatherData.visibility;
-                        data.weather.wind = weatherData.wind;
-                        data.weather.clouds = weatherData.clouds;
-                        data.weather.conditions = weatherData.weather;
-                        data.weather.aluminac = {
-                            sunrise: weatherData.sys.sunrise,
-                            sunset: weatherData.sys.sunset
-                        };
-                        data.weather.coord = weatherData.coord;
-                        data.weather.sys = weatherData.sys;
-                        callback(data);
-                    }).catch(e => console.error(e));
+                $.each(address_components, field => {
+                    //console.log(JSON.stringify(address_components[field], null, 2));
+                    var isPostalCode = (address_components[field].types.indexOf("postal_code") > -1);
+                    var isRegion = (address_components[field].types.indexOf("administrative_area_level_1") > -1);
+                    if (isPostalCode) {
+                        data.postal_code = address_components[field].long_name;
+                    }
+                    if (isRegion) {
+                        data.region = {};
+                        data.region.name = address_components[field].long_name;
+                        data.region.code = address_components[field].short_name;
+                    }
+                });
+                // The formatted address
+                data.formatted_address = geoData.results[0]["formatted_address"];
+                // Get the location's weather conditions
+                $.getJSON('https://fcc-weather-api.glitch.me/api/current?lon=' + data.location.longitude + '&lat=' + data.location.latitude, weatherData => {
+                    console.log(JSON.stringify(weatherData, null, 2));
+                    data.weather = weatherData.main;
+                    data.weather.temp = {
+                        celsius: weatherData.main.temp,
+                        kelvin: (273 + weatherData.main.temp),
+                        fahrenheit: (weatherData.main.temp * 9 / 5 + 32)
+                    };
+                    data.weather.visibility = weatherData.visibility;
+                    data.weather.wind = weatherData.wind;
+                    data.weather.clouds = weatherData.clouds;
+                    data.weather.conditions = weatherData.weather;
+                    data.weather.aluminac = {
+                        sunrise: weatherData.sys.sunrise,
+                        sunset: weatherData.sys.sunset
+                    };
+                    data.weather.coord = weatherData.coord;
+                    data.weather.sys = weatherData.sys;
+                    callback(data);
                 }).catch(e => console.error(e));
             }).catch(e => console.error(e));
         }).catch(e => console.error(e));
