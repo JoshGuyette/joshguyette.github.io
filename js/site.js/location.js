db0f4ce5246693f15aae1dec895665fbbcc406ca
nightness <joshguyettejobs@live.com>
2019-04-29 00:20:00 -0500

    mod

--- js/site.js/location.js
+++ js/site.js/location.js
@@ -1,22 +1,21 @@
 // Promises information about the client's location
-// Requires: jQuery
 function getLocationInformation(apiKey, callback) {
     // Get the client's IP address
-    $.getJSON('//api.ipify.org?format=jsonp&callback=?', data => {
+    $.getJSON('//api.ipify.org?format=jsonp&callback=?').then(ipData => {
         // Get the client's IP based location data
-        $.getJSON('//geoip.nekudo.com/api/' + data.ip, data => {
+        $.getJSON('//geoip.nekudo.com/api/' + ipData.ip).then(data => {
             // Find location details from Google Geocode
-            $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + data.location.latitude + ',' + data.location.longitude + '&key=' + apiKey, geoData => {
+            $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + data.location.latitude + ',' + data.location.longitude + '&key=' + apiKey).then(geoData => {
                 //console.log(JSON.stringify(geoData, null, 2));    
                 var address_components = geoData.results[0]["address_components"];
-                $.each(address_components, field => {
+                address_components.forEach(field => {
                     //console.log(JSON.stringify(address_components[field], null, 2));
                     var isPostalCode = (address_components[field].types.indexOf("postal_code") > -1);
                     var isRegion = (address_components[field].types.indexOf("administrative_area_level_1") > -1);
                     if (isPostalCode) {
                         data.postal_code = address_components[field].long_name;
                     }
-                    if (isRegion) {
+                    else if (isRegion) {
                         data.region = {};
                         data.region.name = address_components[field].long_name;
                         data.region.code = address_components[field].short_name;
@@ -25,8 +24,8 @@ function getLocationInformation(apiKey, callback) {
                 // The formatted address
                 data.formatted_address = geoData.results[0]["formatted_address"];
                 // Get the location's weather conditions
-                $.getJSON('https://fcc-weather-api.glitch.me/api/current?lon=' + data.location.longitude + '&lat=' + data.location.latitude, weatherData => {
-                    console.log(JSON.stringify(weatherData, null, 2));
+                $.getJSON('https://fcc-weather-api.glitch.me/api/current?lon=' + data.location.longitude + '&lat=' + data.location.latitude).then(weatherData => {
+                    //console.log(JSON.stringify(weatherData, null, 2));
                     data.weather = weatherData.main;
                     data.weather.temp = {
                         celsius: weatherData.main.temp,
